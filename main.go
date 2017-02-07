3178e44ac12a8f6f63d8d99b2dfa249e656a6d2a
Ahmad Zidan <me@toeris.my.id>
2017-02-07 14:06:15 +0700

    Fix little issue v:

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" -> " getSimsimi(message.Text))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" -> "+getSimsimi(message.Text))).Do(); err != nil {
 					log.Print(err)
 				}
 			}
